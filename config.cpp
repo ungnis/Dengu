5be2747025d4be01d3e01f336121c27e6ec2b1e2
Михась <41321361+ungnis@users.noreply.github.com>
2022-07-02 18:09:52 +0400

    Add files via upload

--- config.cpp
+++ config.cpp
@@ -4,17 +4,35 @@
 
 const std::set<std::string> vowels =
 {
-    "a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú"
+    "a", "á", 
+    "e", "é", 
+    "i", "í", 
+    "o", "ó", 
+    "u", "ú"
 };
 
 const std::set<std::string> endings = 
 {
-    "as", "is", "os", "us", "om", "ti", "ás", "ís", "ós", "ús", "óm"
+    "as", "ás", 
+    "is", "ís", 
+    "os", "ós", 
+    "us", "ús", 
+    "om", "óm", 
+    "onts", "ónts", 
+    "ti" 
 };
 
 const std::set<std::string> diphtongs = 
 {
-    "ey", "oy", "éy", "óy"
+    "ey", "éy",
+    "oy", "óy",
+    "ew", "éw",
+    "ow", "ów", 
+    "wu", "wú", 
+    "l̥h₁", "ĺ̥h₁",
+    "r̥h₁", "ŕ̥h₁",
+    "m̥h₁", "ḿ̥h₁",
+    "n̥h₁", "ń̥h₁"
 };
 
 std::vector<std::pair<std::string,std::string>> change =
@@ -22,15 +40,24 @@ std::vector<std::pair<std::string,std::string>> change =
     std::make_pair("ʰ", ""),
     std::make_pair("ǵ", "g"),
     std::make_pair("ḱ", "k"),
-    std::make_pair("r̥", "er"),
-    std::make_pair("ŕ̥", "ér"),
+    std::make_pair("l̥h₁", "le"),
+    std::make_pair("ĺ̥h₁", "lé"),
+    std::make_pair("r̥h₁", "re"),
+    std::make_pair("ŕ̥h₁", "ré"),
+    std::make_pair("m̥h₁", "me"),
+    std::make_pair("ḿ̥h₁", "mé"),
+    std::make_pair("n̥h₁", "ne"),
+    std::make_pair("ń̥h₁", "ne"),
     std::make_pair("wl̥", "wol"),
     std::make_pair("wĺ̥", "wól"),
     std::make_pair("l̥", "el"),
     std::make_pair("ĺ̥", "él"),
+    std::make_pair("r̥", "er"),
+    std::make_pair("ŕ̥", "ér"),
     std::make_pair("m̥", "em"),
-    std::make_pair("ḿ̥", "ém̥"),
+    std::make_pair("ḿ̥", "ém"),
     std::make_pair("n̥", "en"),
+    std::make_pair("ń̥", "én"),
     std::make_pair("ʷ", "w"),
     std::make_pair("ti", ""),
     std::make_pair("as", "a"),
@@ -43,11 +70,17 @@ std::vector<std::pair<std::string,std::string>> change =
     std::make_pair("ús", "ú"),
     std::make_pair("om", "o"),
     std::make_pair("óm", "ó"),
+    std::make_pair("onts", "ont"),
+    std::make_pair("ónts", "ónt"),
+    std::make_pair("ā", "a"),
     std::make_pair("ā́", "á"),
-    std::make_pair("ḗ", "é"),
     std::make_pair("ē", "e"),
-    std::make_pair("ṓ", "ó"),
+    std::make_pair("ḗ", "é"),
+    std::make_pair("ī", "i"),
+    std::make_pair("ī́", "í"),
     std::make_pair("ō", "o"),
+    std::make_pair("ṓ", "ó"),
+    std::make_pair("ū", "u"),
     std::make_pair("ū́", "ú"),
     std::make_pair("ey", "i"),
     std::make_pair("éy", "í"),
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <fstream> // std::ifstream, std::ofstream
+#include <iostream> // std::cout, std::cin
 #include <string> // std::string
 #include <algorithm> // std::find
 #include "config.cpp" // change, endings, diphtongs
@@ -10,26 +11,27 @@ bool contains(std::set<std::string> set, std::string element)
 
 bool before_vowel(std::string word, std::string x)
 {
-    return contains(vowels, word.substr(word.find(x)+x.length(), 1)) ||
-           contains(vowels, word.substr(word.find(x)+x.length(), 2));
+    for(int len = 2; len >= 0; --len)
+    {
+        if(word.length() >= word.find(x) + x.length() + len)
+        {
+            return contains(vowels, word.substr(word.find(x) + x.length(), len));
+        }
+    }
+}
+
+void replace_endings(std::string& word, std::string x, std::string y)
+{
+    std::size_t pos = word.length() - x.length();
+    if(word.find(x) == pos && pos != std::string::npos)
+    {
+        word.replace(pos, x.length(), y);
+    }
 }
 
 void replace(std::string& word, std::string x, std::string y)
 {
-    int pos = word.find(x);
-    if(contains(diphtongs, x) && before_vowel(word, x))
-    {
-        return;
-    }
-    if(contains(endings, x))
-    {
-        pos = word.length() - x.length();
-        if(word.find(x) == pos && pos != std::string::npos)
-        {
-            word.replace(pos, x.length(), y);
-        }
-        return;
-    }
+    std::size_t pos = word.find(x);
     while(pos != std::string::npos)
     {
         word.replace(pos, x.length(), y);
@@ -37,38 +39,66 @@ void replace(std::string& word, std::string x, std::string y)
     }
 }
 
+void open(std::ifstream& file, std::string filename)
+{
+    file.open(filename);
+    while(!file.is_open())
+    {
+        std::cout << "Could not open the file " << filename
+        << "\nEnter the correct filename: ";
+        std::cin >> filename;
+        file.open(filename);
+    }
+}
+
 std::string dengu(std::string word)
 {
+    std::string x, y;
     for(auto it = change.begin(); it != change.end(); ++it)
     {
-        replace(word, it->first, it->second);
+        x = it->first;
+        y = it->second;
+        if(contains(endings, x))
+        {
+            replace_endings(word, x, y);
+        }
+        else if(!(contains(diphtongs, x) && before_vowel(word, x)))
+        {
+            replace(word, x, y);
+        }
     }
     return word;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::ifstream dic;
-    std::ofstream out;
+    const std::string home = std::getenv("HOME");
+    const std::string dir = "/prog/cpp/dengu/pie";
+    const std::string in = (argc > 1) ? argv[1] : home + dir;
+    const std::string out = (argc > 2) ? argv[2] : in.substr(in.rfind("/") + 1, std::string::npos) + "_dengu";
+    const std::string mark = (argc > 3) ? argv[3] : "*";
+    const std::string del = (argc > 4) ? argv[4] : " —";
+    const int mark_len = mark.length();
+    std::ifstream input;
+    std::ofstream output;
+    open(input, in);
+    output.open(out);
     std::string line, word;
     std::size_t begin, end;
-    dic.open("swadesh");
-    out.open("out");
-    while(std::getline(dic, line))
+    while(std::getline(input, line))
     {
-        begin = line.find("*");
+        begin = line.find(mark);
         while(begin != std::string::npos)
         {
-            line.erase(begin, 1);
-            end = line.find(" ", begin) - begin;
+            line.erase(begin, mark_len);
+            end = line.find(del, begin) - begin;
             word = line.substr(begin, end);
-            word = word.substr(0, word.find(","));
             line.replace(begin, word.length(), dengu(word) + " (" + word + ")");
-            begin = line.find("*");
+            begin = line.find(mark);
         }
-        out << line << std::endl;
+        output << line << std::endl;
     }
-    dic.close();
-    out.close();
+    input.close();
+    output.close();
     return 0;
 }
